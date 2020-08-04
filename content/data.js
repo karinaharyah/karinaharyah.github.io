c50db6c239334ea6667efc7bfce11482efbaa93b
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2020-08-04 15:33:38 -0300

    Initial Commit

---
+++ content/data.js
@@ -0,0 +1,84 @@
+const profile = {
+  "about": {
+    "name": "Karina",
+    "surname": "Costa Baptista",
+    "email": "karinaharyah@gmail.com",
+    "contact": "Esteticista / Cosmetologa · +55 (11) 94473-6657 · ",
+    "description": "Vida profissional pautada em grandes desafios. Sendo esta, a oportunidade ideal para demonstrar e desenvolver ainda mais habilidades e competências, absorver novos conhecimentos, e exercer o trabalho em equipe.",
+    "networks": {
+      "fab fa-linkedin-in": "https://www.linkedin.com/in/karina-costa-baptista-694988106/",
+      "fab fa-instagram": "https://instagram.com/est.karinacosta?igshid=1v6jx3144jp42",
+      "fab fa-whatsapp": "https://wa.me/qr/YPI4BCZB6RVID1",
+    }
+  },
+  "interests": [
+    "Formada em Estética e Cosmetologia pela UNINOVE,  desde os 17 anos atuando  no segmento de cosméticos, tenho grande apreço pela área de treinamentos, adoro a sensação de levar conhecimento e informações  relevantes para pessoas , gosto muito de estudar sobre a anatomia da pele, e a  sua interação com dermocosméticos e cosmecêuticos. Pretendo me especializar em Cosmetologia, para adquirir novos conhecimentos relacionados à formulação e composição de produtos."
+  ],
+  "tech": "Estética e Cosmética: Embasamento teórico sobre anatomia, fisiopatologia e disfunções da pele, avaliação corporal, biossegurança, procedimentos e protocolos, aplicação de eletroterapia, microagulhamento, cosmecêuticos e dermocosméticos, princípios ativos e suas aplicações, aromaterapia e OE’s, noções básicas de massoterapia. Habilidades com rotinas administrativas: Experiência em alimentação e elaboração de planilhas, recebimento e envio de malotes, arquivação de documentos e Notas Fiscais, recebimento e transferência de mercadorias, agendamento de processos com empresas parceiras, controle de validade de produtos, análise de mercado (combate à concorrência), pedidos de materiais de escritório, auxílio a prevenção de perdas. Conhecimentos em design gráfico, wordpress (HTML, CSS - componentes customizados/widgets), UI/GUI, elaboração de arte vetorial, identidade visual, logotipo, cartão de visitas, cartilhas, cartazes, entre outros. Inglês avançado , com ênfase em escrita e leitura - Prática e comunicação diária com estrangeiros. Proativa, centrada e organizada.",
+  "education": {
+    "Universidade Nove de Julho": {
+      "subheading": "Estética e Cosmética",
+      "description": "O curso de Tecnologia em Estética e Cosmética busca capacitar o futuro profissional para identificar desordens estéticas, planejar e aplicar técnicas e métodos preventivos e reparadores em tratamentos faciais e corporais, por meio da utilização de novas tecnologias da área, visando a saúde e bem-estar do paciente/cliente. Além disso, o tecnólogo torna-se qualificado para empreender o próprio negócio ou gerenciar empresas no setor.",
+      "timeframe": "Técnologo - Completo </br> Janeiro 2018 - Julho 2020"
+    },
+    "ETEC Zona Leste": {
+      "subheading": "Ensino Médio",
+      "description": "",
+      "timeframe": "Colegial - Completo </br> Janeiro 2011 - Dezembro 2013"
+    }
+  },
+  "experiences": {
+    "Ito Andrade Cabeleireiros": {
+      "subheading": "Auxiliar Administrativo",
+      "description": "Atuei no setor receptivo e administrativo, principalmente no atendimento ao público, desenvolvendo as funções de agendamento de clientes, fluxo de caixa, controle de estoque, mala direta, redes sociais.",
+      "timeframe": "Maio 2019 – Agosto 2019"
+    },
+    "Beauty Box": {
+      "subheading": "Auxiliar Administrativo / Curadora",
+      "description": "Atuação na área comercial, desempenhando rotinas administrativas da loja, planilhas de controle de validade, recebimento e gerenciamento de notas fiscais, requisição de mercadoria. Controle e emissão de documentação fiscal utilizando sistema LINX, montagem de ciclos e material de VM. Abertura de chamados, transferências, controle de documentos. Mediante à Promoção, tive a oportunidade de maior atuação na área comercial, com ênfase em vendas, atendimento ao público, prospecção e fidelização de clientes, organização de layout, montagem de ciclos e VM. Com grande vivência na área de vendas e conhecimento em Share de fragrâncias e marcas premium.",
+      "timeframe": "Março 2017 - Novembro 2018"
+    },
+    "Drogaria Pacheco": {
+      "subheading": "Atendimento ao Cliente",
+      "description": "No ano de 2016, obtive a oportunidade de atuar na área de atendimento ao cliente com foco em dermo consultoria e perfumaria, abertura e fechamento de caixa, dispensação de medicamentos, abastecimento de mercadorias e gerenciamento de estoque.",
+      "timeframe": "Maio 2016 - Novembro 2016"
+    },
+    "Farmácia Mini Preço": {
+      "subheading": "Atendente de Balcão",
+      "description": "Em 2015, atuei na área comercial, onde realizava o controle de dispensação de medicamentos, orientações de uso, gerenciamento de estoque e controle de validade, abertura e fechamento de caixa, atuando também no recebimento e controle de notas fiscais do estabelecimento. ",
+      "timeframe": "Novembro 2015 - Fevereiro 2016 "
+    },
+    "Drogaria Onofre Megastore Paulista": {
+      "subheading": "Auxiliar de Loja",
+      "description": "Em 2014, atuei na função de aprendiz, onde desenvolvi atividades como atendimento ao cliente, consultoria de dermocosméticos, abertura e fechamento de caixa, PVPS, controle de validade, gerenciamento de estoque e exposição estratégica de mercadorias. Posteriormente mediante efetivação, adquirindo também funções de requisição de mercadoria e atendimento em balcão de medicamentos.",
+      "timeframe": "Maio 2014 - Junho 2015"
+    }
+  },
+  "certifications" : [
+    "Aromaterapia e suas aplicações - Imazon",
+    "Introdução à Fisioterapia Dermato-Funcional - Imazon",
+    "Introdução à Fisioterapia Desportiva - Imazon",
+    "Biologia - digestão e respiração - Fundação Bradesco Escola Virtual",
+    "Biologia - alimentação e saúde - Fundação Bradesco Escola Virtual",
+    "Fundamentos do MKT Digital - Anhembi-Morumbi",
+    "Marketing Pessoal e Corporativo - Anhembi-Morumbi",
+    "HTML Básico - Fundação Bradesco Escola Virtual",
+    "Atendimento ao Público - Fundação Bradesco Escola Virtual",
+    "Introdução à administração - Fundação Bradesco Escola Virtual",
+    "Excel intermediário - Fundação Bradesco Escola Virtual",
+    "English Review - Fundação Bradesco Escola Virtual",
+    "Verbs in the future - Fundação Bradesco Escola Virtual",
+    "English Adjectives - Fundação Bradesco Escola Virtual",
+    "Logística e Vendas para aprendiz - Isbet",
+    "Operações Financeiras - e-Learning Aero Grupo Boticario",
+    "Acessibilidade - e-Learning Aero Grupo Boticario",
+    "Cosmetologia - Vitaderm",
+    "Treinamento em Marcas Premium de Fragrâncias",
+    "Skin Care",
+    "Makeup e Dermocosméticos"
+  ],
+  "skills": {
+    "icons": [],
+    "workflow": []
+  }
+}
